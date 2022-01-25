e82d032f27179745f283ce1de557a81c6a08ce9c
Isaac Kim <myong328@gmail.com>
2022-01-24 17:40:47 -0800

    Initial Commit

---
+++ weather2App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weather2App
+//
+//  Created by Isaac Kim on 1/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
